b072f49e4fb55af1333606b6b5315782850d4586
henriquewallace <henrique.wallace@hotmail.com>
2022-06-23 11:59:02 -0300

    AUTEST - Refactor first it() method parameter to be dinamic

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -8,12 +8,14 @@ describe(UniqueIdService.name, () => {
     service = new UniqueIdService();
   });
 
-  it('should generate id when called with prefix', () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should generate id when called with prefix`, () => {
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
   });
 
-  it('should not generate duplicated ids when called multiple times', () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should not generate duplicated ids when called multiple times`, () => {
     const ids = new Set();
     for(let i = 0; i < 50; i++) {
       ids.add(service.generateUniqueIdWithPrefix('app'));
@@ -21,7 +23,8 @@ describe(UniqueIdService.name, () => {
     expect(ids.size).toBe(50);
   });
 
-  it('should return the number of generated Ids', () => {
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should return the number of generated Ids`, () => {
     service.generateUniqueIdWithPrefix('app');
     service.generateUniqueIdWithPrefix('app');
     expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
