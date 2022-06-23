cda86aab13fab769bc6c66ee742e0808a9723946
henriquewallace <henrique.wallace@hotmail.com>
2022-06-23 11:17:14 -0300

    AUTEST - Create first test

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,16 +1,9 @@
-import { TestBed } from '@angular/core/testing';
-
 import { UniqueIdService } from './unique-id.service';
 
-describe('UniqueIdService', () => {
-  let service: UniqueIdService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UniqueIdService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+describe(UniqueIdService.name, () => {
+  it('should generate id when called with prefix', () => {
+    const service = new UniqueIdService();
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id.startsWith('app-')).toBeTrue();
   });
 });
